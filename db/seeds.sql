9bb0b6652043b98f2856b5330e047c8e0c2d8685
Moniqueemh <mohernandez9992@yahoo.com>
2020-12-14 19:32:15 -0800

    added seeds

---
+++ db/seeds.sql
@@ -0,0 +1,12 @@
+INSERT INTO burgers
+    (burger_name, devoured)
+VALUES
+    ('Skinny Turkey Burger', false);
+    INSERT INTO burgers
+    (burger_name, devoured)
+VALUES
+    ('Americana Cheeseburger', false);
+    INSERT INTO burgers
+    (burger_name, devoured)
+VALUES
+    ('Messy Stuffed Burger', false);
