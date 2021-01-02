d484ecde51f708f01f322553532506aad8ca5cb0
Moniqueemh <mohernandez9992@yahoo.com>
2021-01-01 17:53:38 -0800

    setup connection node to mysql

---
+++ config/connection.js
@@ -0,0 +1,27 @@
+// Set up MySQL connection
+const mysql = require("mysql");
+var connection;
+
+//Makes connection
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: "Jackjack0307!",
+        database: "burgers_db"
+    });
+};
+
+connection.connect(function (err) {
+    if (err) {
+        console.error("error connecting: " + err.stack);
+        return;
+    }
+    console.log("connected as id " + connection.threadId);
+});
+
+//Exports connection for ORM to use
+module.exports = connection;
