864ecf3fc5ace45951cfa1600dca00b670465792
Moniqueemh <mohernandez9992@yahoo.com>
2021-01-02 00:15:25 -0800

    created code that calls orm

--- models/burger.js
+++ models/burger.js
@@ -1,2 +1,24 @@
 // import ORM
 const orm = require("../config/orm.js");
+
+const burgers = {
+    all: function (cb) {
+        orm.selectAll("burgers", function (result) {
+            cb(result);
+        });
+    },
+    // The variables cols and vals are arrays.
+    create: function (cols, vals, cb) {
+        orm.insertOne("burgers", col, val, function (result) {
+            cb(result);
+        });
+    },
+    update: function (col, val, id, cb) {
+        orm.updateOne("burgers", col, val, id, function (result) {
+            cb(result);
+        });
+    },
+};
+
+// Export the database functions for the controller.
+module.exports = burgers;
