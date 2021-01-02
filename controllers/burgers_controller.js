6fb6da6adf97ec51d76d5f9f9d1c064d9690a3ac
Moniqueemh <mohernandez9992@yahoo.com>
2021-01-02 00:47:18 -0800

    created all routes for server.js

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const router = express.Router();
+const burger = require("../models/burger.js");
+
+// Create all our routes and set up logic within those routes where required.
+router.get("/", function (req, res) {
+    burgers.all(function (data) {
+        const hbsObject = {
+            burgers: data
+        };
+        console.log(hbsObject);
+        res.render("index", hbsObject);
+    });
+});
+
+router.post("/api/burgers", function (req, res) {
+    burgers.create([
+        "burger_name"
+    ], [
+        req.body.name,
+    ], function (result) {
+        // Send back the ID of the new quote
+        res.json({ id: result.insertId });
+    });
+});
+
+router.put("/api/burgers/:id", function (req, res) {
+    const condition = "id = " + req.params.id;
+
+    console.log("condition", condition);
+
+    burgers.update({
+        "devoured": req.params.id
+    }, function (result) {
+        if (result.changedRows == 0) {
+            // If no rows were changed, then the ID must not exist, so 404
+            return res.status(404).end();
+        } else {
+            res.status(200).end();
+        }
+    });
+});
+
+// Export routes for server.js to use.
+module.exports = router;
